8067b4132d9362cc10824c98064fbbfa29ca60b9
TswelopeleMojewa-SQL-DEV <tswelopele@payday.co.za>
2025-10-20 14:22:26 +0200

    Allowed the backend to access all the origins.

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use('/uploads', express.static('uploads'));
 
 // ✅ Enable CORS for your frontend
 app.use(cors({
-  origin: "http://localhost:5173", // Allow your React app
+  origin: "*", // Allow your React app // http://localhost:5173
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
